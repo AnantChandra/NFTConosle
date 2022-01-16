d1307d0647a47127f45661c2f5821efadd3e074d
Anant Chandra <anantcha@usc.edu>
2022-01-16 13:29:39 +0100

    Create form to accept and upload image, log and retrieve the same from IPFS using hash generated
    Currently IPFS is timing out in the retrieval part, possibly due to the low availability of the service

--- client/src/App.js
+++ client/src/App.js
@@ -1,66 +1,91 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./getWeb3";
+import ipfs from './ipfs';
 
 import "./App.css";
+import { read } from "fs";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      storageValue: 0,
+      web3: null,
+      contract: null,
+      buffer: null,
+      ipfsHash: null
+    }
+    this.captureFile = this.captureFile.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
       const web3 = await getWeb3();
+      console.log("web3", web3);
+
+      this.setState({
+        web3: web3
+      })
 
       // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+      const accounts = web3.eth.getAccounts();
 
       // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
+      const networkId = web3.eth.net.getId();
       const deployedNetwork = SimpleStorageContract.networks[networkId];
       const instance = new web3.eth.Contract(
         SimpleStorageContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({
+        contract: instance
+      })
     } catch (error) {
       // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
       console.error(error);
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+  captureFile(event) {
+    console.log('Capture file...');
+    event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) })
+      console.log('buffer', this.state.buffer);
+    }
+  }
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
+  onSubmit(event) {
+    event.preventDefault()
+    console.log('On submit...');
+    ipfs.files.add(this.state.buffer, (err, result) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      this.setState({ ipfsHash: result[0].hash })
+      console.log('ipfsHash', this.state.ipfsHash)
+    })
+  }
 
   render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
+        <h1>Your Image!</h1>
+        <p>The image is stored in IPFS & The Ethereum Blockchain!</p>
+        <img src={`https://gateway.pinata.cloud/ipfs/${this.state.ipfsHash}`} alt="" />
+        <h2>Upload Image</h2>
+        <form onSubmit={this.onSubmit}>
+          <input type='file' onChange={this.captureFile} />
+          <input type='submit' />
+        </form>
         <p>
           Try changing the value stored on <strong>line 42</strong> of App.js.
         </p>
